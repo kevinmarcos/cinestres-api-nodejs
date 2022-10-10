5d10d9851390373065ee042e1d540119eba0623f
kevinmarcos <81602436+kevinmarcos@users.noreply.github.com>
2022-10-09 21:45:26 -0500

    3er commit

--- src/app.js
+++ src/app.js
@@ -27,11 +27,15 @@ app.use(
     compRealizadas
 )
 
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://cinestres-api-nodejs-production.up.railway.app/"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use((req, res, next) => {
     res.status(404).json({
-        message:'Endpoint not found'
+        message: 'Endpoint not found'
     })
 })
 
